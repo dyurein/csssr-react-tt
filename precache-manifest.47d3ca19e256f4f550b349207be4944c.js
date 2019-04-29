296a12c78e61cd3893725df15c1de1835ff86cdb
Sergei <sergejgaponov713@gmail.com>
2019-04-29 23:23:31 +0400

    Updates

---
+++ precache-manifest.47d3ca19e256f4f550b349207be4944c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d501dd35bb9b623faaf3fefd934353a",
+    "url": "/dyurein/csssr-react-tt/index.html"
+  },
+  {
+    "revision": "b158810cdd091a3a834d",
+    "url": "/dyurein/csssr-react-tt/static/css/2.e16c2e2d.chunk.css"
+  },
+  {
+    "revision": "d945fe99f1aaaa246b79",
+    "url": "/dyurein/csssr-react-tt/static/css/main.2f0f7bbe.chunk.css"
+  },
+  {
+    "revision": "b158810cdd091a3a834d",
+    "url": "/dyurein/csssr-react-tt/static/js/2.a8d3a104.chunk.js"
+  },
+  {
+    "revision": "d945fe99f1aaaa246b79",
+    "url": "/dyurein/csssr-react-tt/static/js/main.1978b9a2.chunk.js"
+  },
+  {
+    "revision": "81342f4f8c19300e7a3e",
+    "url": "/dyurein/csssr-react-tt/static/js/runtime~main.93b4ccb2.js"
+  },
+  {
+    "revision": "a3b387c93882604792867736aecd56c8",
+    "url": "/dyurein/csssr-react-tt/static/media/Montserrat-Bold.a3b387c9.ttf"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/dyurein/csssr-react-tt/static/media/Montserrat-Regular.a8a11736.ttf"
+  },
+  {
+    "revision": "91b510609446facfd45651b6c7460f5e",
+    "url": "/dyurein/csssr-react-tt/static/media/PT_Serif-Regular.91b51060.ttf"
+  },
+  {
+    "revision": "c1741ae28d222a9febc2f9ee3463be79",
+    "url": "/dyurein/csssr-react-tt/static/media/bulb.c1741ae2.svg"
+  },
+  {
+    "revision": "5ead9985556c540e03d972df836c5ced",
+    "url": "/dyurein/csssr-react-tt/static/media/compass.5ead9985.svg"
+  },
+  {
+    "revision": "82500e0dc8f651ac1a0849c8fc32cf07",
+    "url": "/dyurein/csssr-react-tt/static/media/couture-bld.82500e0d.otf"
+  },
+  {
+    "revision": "11f2de1356eae47a9589233d36ed0980",
+    "url": "/dyurein/csssr-react-tt/static/media/heart.11f2de13.svg"
+  },
+  {
+    "revision": "f7f6ee9fb10ecb70a7e767d373fa424c",
+    "url": "/dyurein/csssr-react-tt/static/media/main-section.f7f6ee9f.jpg"
+  },
+  {
+    "revision": "9706a2d19574e50e7fb41b31406cf11f",
+    "url": "/dyurein/csssr-react-tt/static/media/photo_1.9706a2d1.png"
+  },
+  {
+    "revision": "b26345712847497ab5c0740e3d58f9d1",
+    "url": "/dyurein/csssr-react-tt/static/media/photo_2.b2634571.png"
+  },
+  {
+    "revision": "3574c5eb3c65e41f9842221bd18adc0c",
+    "url": "/dyurein/csssr-react-tt/static/media/photo_3.3574c5eb.png"
+  },
+  {
+    "revision": "b38c02a0e59ad741e3104af32532bc23",
+    "url": "/dyurein/csssr-react-tt/static/media/photo_4.b38c02a0.png"
+  },
+  {
+    "revision": "1031b23cf3081e1420d1d4ba3e35447f",
+    "url": "/dyurein/csssr-react-tt/static/media/photo_5.1031b23c.png"
+  },
+  {
+    "revision": "8b88219dfc0a0f0f9beec3273baea4e7",
+    "url": "/dyurein/csssr-react-tt/static/media/photo_6.8b88219d.png"
+  },
+  {
+    "revision": "3cc52291fbd100e2b201184c8e3cded7",
+    "url": "/dyurein/csssr-react-tt/static/media/photo_8.3cc52291.png"
+  },
+  {
+    "revision": "2f8a9a480cbbba1e7e36cbb382e552f1",
+    "url": "/dyurein/csssr-react-tt/static/media/tools.2f8a9a48.svg"
+  }
+]);
